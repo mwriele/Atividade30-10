dc4293def42296cfe736894dfb5c2e1bed9761dc
Mariele Evangelista <164822586+mwriele@users.noreply.github.com>
2024-10-30 15:01:43 -0300

    Exerc 2 - Classe Retangulo.java
    Classe Retangulo para calcular área e perímetro.

---
+++ Exerc 2 - Classe Retangulo.java
@@ -0,0 +1,19 @@
+package listas.lista01;
+
+public class Retangulo {
+    private double base;
+    private double altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+}
